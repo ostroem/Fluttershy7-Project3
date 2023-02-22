71ce7648824fc7e0a34ae697ec39dc1b371dc936
Lesstread <126073337+Lesstread@users.noreply.github.com>
2023-02-22 15:42:06 +0100

    hulahoop falling, vlad scene

---
+++ Assets/Scripts/Hulahoop_Falling.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hulahoop_Falling : MonoBehaviour
+{
+    [SerializeField] GameObject[] HoopPrefab;
+    [SerializeField] float nextSpawn = 1.5f;
+    [SerializeField] float minRange;
+    [SerializeField] float maxRange;
+   //[SerializeField] int framesPerTime = 10; // number of frames per time unit
+   [SerializeField] float fallSpeed = 1f; // adjustable speed for falling object
+
+
+    void Start()
+    {
+        StartCoroutine(HoopSpawn());
+    }
+
+
+    IEnumerator HoopSpawn()
+    {
+        while (true)
+        {
+            var wanted = Random.Range(minRange, maxRange);
+            var position = new Vector3(wanted, transform.position.y);
+
+            GameObject gameObject = Instantiate(HoopPrefab[Random.Range(0, HoopPrefab.Length)], position, Quaternion.identity);
+            gameObject.transform.position += new Vector3(0, -fallSpeed * Time.deltaTime, 0); // move object down by fallSpeed per frame
+
+            yield return new WaitForSeconds(nextSpawn);
+            Destroy(gameObject, 5f);
+        }
+    }
+
+}
