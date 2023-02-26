35d74cfc29b73f70280882c9845a5bee3e51281b
Oliver <77832524+ostroem@users.noreply.github.com>
2023-02-26 16:17:18 +0100

    work, i don't know exactly
    Oliver

--- Assets/Oliver/Scripts/Player.cs
+++ Assets/Oliver/Scripts/Player.cs
@@ -2,57 +2,132 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Player : MonoBehaviour
 {
 
-    [SerializeField] int tile_size;
-    [SerializeField] int score = 0;
-    [SerializeField] float movement_speed;
     [SerializeField] private int totalHulasEquipped = 0;
+    [SerializeField] int score = 0;
+    [SerializeField] int timesShaken = 0;
+    [SerializeField] int pointsForColorCombo = 10;
+    [SerializeField] private TMP_Text scoreText;
+    private float movement_speed = 5;
     private Vector3 target_position;
     private Animator animator;
     bool isShaking = false;
-    float elapsedTimeSinceShake = 0.0f;
-    [SerializeField] int timesShaken = 0;
+    float elapsedTimeSinceLastShake = 0.0f;
+    float timeToRemoveHula = 0.0f;
+    List<Hulahoop> hulas;
+    public int TotalHulas { get => totalHulasEquipped; }
+    public bool easyCollision = false;
 
     private void Awake()
     {
         target_position = transform.position;
         animator = GetComponent<Animator>();
+        hulas = new List<Hulahoop>();
     }
 
     void Update()
     {
         movement();
         shake_body();
+        checkHulas();
+        update_score_text();
+
+        if (want_to_remove_hula())
+        {
+            remove_last_hula();
+        }
+    }
+
+    private void update_score_text()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    private void checkHulas()
+    {
+        if(totalHulasEquipped >= 3)
+        {
+            if (hulas.TrueForAll(i => i.type == Hulahoop.Type.Pink))
+            {
+                score += pointsForColorCombo;
+                animator.SetTrigger("removeAllPink");
+                totalHulasEquipped = 0;
+                hulas.Clear();
+            }
+            else if (hulas.TrueForAll(i => i.type == Hulahoop.Type.Red))
+            {
+                score += pointsForColorCombo;
+                animator.SetTrigger("removeAllRed");
+                totalHulasEquipped = 0;
+                hulas.Clear();
+            }
+            else if (hulas.TrueForAll(i => i.type == Hulahoop.Type.Red))
+            {
+                score += pointsForColorCombo;
+                animator.SetTrigger("removeAllViolet");
+                totalHulasEquipped = 0;
+                hulas.Clear();
+            }
+        }
     }
 
     public bool GetIsShaking() { return isShaking; }
     private void shake_body()
     {
-        if (Input.GetKeyDown(KeyCode.Space) )
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             isShaking = true;
             animator.SetTrigger("shakingBody");
-            elapsedTimeSinceShake = 0.0f;
+            elapsedTimeSinceLastShake = 0.0f;
+
             timesShaken++;
         }
-        else {
-            elapsedTimeSinceShake += Time.deltaTime;
-            if(elapsedTimeSinceShake >= 0.32f)
+        else
+        {
+            if (easyCollision)
             {
-                timesShaken = 0;
-                isShaking = false;
+                return;
             }
 
-        }
 
-        if(timesShaken > 2)
-        {
-            totalHulasEquipped = 2;
-            animator.SetTrigger("removeHula");
+            elapsedTimeSinceLastShake += Time.deltaTime;
+            timeToRemoveHula += Time.deltaTime;
+            if (timeToRemoveHula > 0.5f)
+            {
+                timeToRemoveHula = 0.0f;
+                timesShaken = 0;
+            }
+            if (elapsedTimeSinceLastShake >= 1.6f)
+            {
+                elapsedTimeSinceLastShake = 0;
+                totalHulasEquipped = 0;
+                animator.SetTrigger("stoppedShaking");
+
+                hulas.Clear();
+                isShaking = false;
+            }
         }
+    }
+
+    private void remove_last_hula()
+    {
+        Debug.Log("remove last hula");
+        timesShaken = 0;
+        totalHulasEquipped--;
+        animator.SetTrigger("removeHula");
+        hulas.RemoveAt(hulas.Count - 1);
+    }
+
+    private bool want_to_remove_hula()
+    {
+        if (timesShaken > 2 && totalHulasEquipped >= 3)
+        {
+            return true;
+        }
+        else return false;
     }
 
     private void movement()
@@ -69,7 +144,7 @@ public class Player : MonoBehaviour
 
         if (IsOutOfBounds(target_position))
         {
-            target_position.x = Mathf.FloorToInt(transform.position.x);
+            target_position.x = Mathf.RoundToInt(transform.position.x);
             return;
         }
         Vector3 new_pos = Vector3.MoveTowards(transform.position, target_position, Time.deltaTime * movement_speed);
@@ -101,13 +176,18 @@ public class Player : MonoBehaviour
             return false;
     }
 
-    public void SetAnimation(Hulahoop.Type type)
+    public void SetAnimation(Hulahoop hula)
     {
-        if(totalHulasEquipped < 3)
+        if(totalHulasEquipped >= 3)
         {
-            totalHulasEquipped++;
+            Debug.Log("too many hulas");
+            return;
         }
-        switch (type)
+
+        totalHulasEquipped++;
+        hulas.Add(hula);
+
+        switch (hula.type)
         {
             case Hulahoop.Type.Pink:
                 animator.SetBool("collideWithPink", true);
